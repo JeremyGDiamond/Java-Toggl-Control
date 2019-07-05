32e0c52124d3301561f49f62356c8d18d31c0dfd
JeremyGDiamond <jeremygdiamond@gmail.com>
2019-07-04 20:35:21 -0600

    moving main to another pacakge for future inclution in other applications

--- JavaTogglControl/src/TestMain/Main.java
+++ JavaTogglControl/src/TestMain/Main.java
@@ -3,20 +3,28 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package toggl_api_interface;
+package TestMain;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.ProtocolException;
 import java.util.ArrayList;
+import toggl_api_interface.togglAccount;
 
 
 /**
  *
  * @author jeremy
  */
-public class Test_Main {
+public class Main {
     public static void main(String[] args) throws ProtocolException, IOException {
         
         //make variables for testing
